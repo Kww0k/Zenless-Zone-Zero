a151ef851e19c842ed8dd60413406a49ccfcdd30
Sparkle <1364795855@qq.com>
2024-11-19 16:36:35 +0800

    个人中心整体布局

--- frontend/src/views/home/components/level/components/LevelPage.vue
+++ frontend/src/views/home/components/level/components/LevelPage.vue
@@ -3,7 +3,22 @@
 </script>
 
 <template>
-1
+  <div style="width: 100%;box-sizing: border-box;border-radius: 36px;border: 6px solid black;height: 100%;background: black">
+    <div style="width: 100%;box-sizing: border-box;border-radius: 36px;border: 6px solid #313131;height: 100%;overflow: hidden;padding: 10px">
+      <div style="height: 35%;width: 100%;position: relative;border-top-left-radius: 36px;border-top-right-radius: 36px;overflow: hidden">
+
+        <img style="width: 100%; height: 100%;object-fit: cover">
+      </div>
+      <div style="height: 10%;display: flex;align-items: center;font-weight: bold;font-size: 18px;">
+        您参与的活动
+      </div>
+      <div style="height: 55%;width: 100%">
+        <div style="width: 100%;border-bottom-left-radius: 36px;border-bottom-right-radius: 36px;background: #313131;height: 100%">
+
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
